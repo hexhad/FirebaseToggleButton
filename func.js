610c0430328f3bfe4bdf3338479d788309362a33
Hashan <hexhad@gmail.com>
2020-09-25 23:13:36 +0530

    Add Comments
    Added proper guidance

--- func.js
+++ func.js
@@ -1,14 +1,19 @@
-
+//access button from id
 const btn = document.getElementById('toggle');
+// jus variable for db
 const database = firebase.database();
 
 
-
+//retriving data from Firebase
+//In my case get the value == 0 or 1 
 database.ref('value').on('value', function(snapshot) {
+    //button click event
     btn.addEventListener('click',(e)=>{
         var val = snapshot.val();
         //window.alert(val);
+        //checking current Firebase value == 0
         if(val === 0){
+            //if value == 0 then change (write on firebase) to 1
             database.ref('/value/').set(1);
         } else {
             database.ref('/value/').set(0);       
