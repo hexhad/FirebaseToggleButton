3c40d1be24854a3a34db5f2e999eaecef74b2134
Hashan <hexhad@gmail.com>
2020-09-25 23:05:26 +0530

    Updated
    Comments

--- index.php
+++ index.php
@@ -11,7 +11,15 @@
     
 <!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/7.21.0/firebase-app.js"></script>
+    
+    
+    
+<!--you will not get this js url from firebase cdn-->
 <script src="https://www.gstatic.com/firebasejs/7.21.0/firebase-database.js"></script>
+    
+    
+    
+    
 
 <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
@@ -21,14 +29,7 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyBkWUGpGw37dPQLySwpaArwM4IZ9g4V8UA",
-    authDomain: "firephpconnection.firebaseapp.com",
-    databaseURL: "https://firephpconnection.firebaseio.com",
-    projectId: "firephpconnection",
-    storageBucket: "firephpconnection.appspot.com",
-    messagingSenderId: "782479023960",
-    appId: "1:782479023960:web:d661db94acbfceffd5cf2d",
-    measurementId: "G-MYLFYF9R4J"
+    Pleas copy relevent codes from the firebase project settings>>Firebase SDK snippet>>CDN
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
